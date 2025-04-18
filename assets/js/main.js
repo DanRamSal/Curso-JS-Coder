d38294d2b8bdd9a2e9815e3e503e4789746cb23b
DanRamSal <danrmz.ibanez@gmail.com>
2025-04-17 22:06:53 -0600

    funciones pagina checkout

--- assets/js/main.js
+++ assets/js/main.js
@@ -5,70 +5,70 @@ const masterProd = [
         nombre: "Blue eyes",
         descripcion: "This legendary dragon is a powerful engine of destruction. Virtually invincible, very few have faced this awesome creature and lived to tell the tale.",
         precio: 25.99,
-        imagen: "assets/images/Prod1.jpg",
+        imagen: "../assets/images/Prod1.jpg",
     },
     {
         id: 2,
         nombre: "Dark Magician",
         descripcion: "The ultimate wizard in terms of attack and defense.",
         precio: 85.27,
-        imagen: "assets/images/Prod2.jpg",
+        imagen: "../assets/images/Prod2.jpg",
     },
     {
         id: 3,
         nombre: "Red eyes",
         descripcion: "A ferocious dragon with a deadly attack.",
         precio: 123.99,
-        imagen: "assets/images/Prod3.jpg",
+        imagen: "../assets/images/Prod3.jpg",
     },
     {
         id: 4,
         nombre: "Avatar",
         descripcion: "By sending 1 'Pot of Greed' from your hand to the Graveyard, draw 3 cards from your Deck.",
         precio: 44.27,
-        imagen: "assets/images/Prod4.jpg",
+        imagen: "../assets/images/Prod4.jpg",
     },
     {
         id: 5,
         nombre: "Dark Paladin",
         descripcion: " Must be Fusion Summoned. When a Spell Card is activated (Quick Effect): You can discard 1 card; negate the activation, and if you do, destroy it. This card must be face-up on the field to activate and to resolve this effect. Gains 500 ATK for each Dragon monster on the field and in the GY.",
         precio: 98.89,
-        imagen: "assets/images/Prod5.jpg",
+        imagen: "../assets/images/Prod5.jpg",
     },
     {
         id: 6,
         nombre: "Mewtwo 1",
         descripcion: "Mewtwo (Japanese: ミュウツー Mewtwo) is a Psychic-type Legendary Pokémon introduced in Generation I. While it is not known to evolve into or from any other Pokémon, Mewtwo can Mega Evolve into two different forms.",
         precio: 45.46,
-        imagen: "assets/images/Prod6.jpg",
+        imagen: "../assets/images/Prod6.jpg",
     },
     {
         id: 7,
         nombre: "Mewtwo 2",
         descripcion: "Mewtwo (Japanese: ミュウツー Mewtwo) is a Psychic-type Legendary Pokémon introduced in Generation I. While it is not known to evolve into or from any other Pokémon, Mewtwo can Mega Evolve into two different forms.",
         precio: 45.46,
-        imagen: "assets/images/Prod7.jpg",
+        imagen: "../assets/images/Prod7.jpg",
     },
     {
         id: 8,
         nombre: "Mewtwo 3",
         descripcion: "Mewtwo (Japanese: ミュウツー Mewtwo) is a Psychic-type Legendary Pokémon introduced in Generation I. While it is not known to evolve into or from any other Pokémon, Mewtwo can Mega Evolve into two different forms.",
         precio: 1245.46,
-        imagen: "assets/images/Prod8.jpg",
+        imagen: "../assets/images/Prod8.jpg",
     },
     {
         id: 9,
         nombre: "Mewtwo 4",
         descripcion: "Mewtwo (Japanese: ミュウツー Mewtwo) is a Psychic-type Legendary Pokémon introduced in Generation I. While it is not known to evolve into or from any other Pokémon, Mewtwo can Mega Evolve into two different forms.",
         precio: 91.46,
-        imagen: "assets/images/Prod9.jpg",
+        imagen: "../assets/images/Prod9.jpg",
     },
     {
         id: 10,
         nombre: "Mewtwo 5",
         descripcion: "Mewtwo (Japanese: ミュウツー Mewtwo) is a Psychic-type Legendary Pokémon introduced in Generation I. While it is not known to evolve into or from any other Pokémon, Mewtwo can Mega Evolve into two different forms.",
         precio: 55.46,
-        imagen: "assets/images/Prod10.jpg",
+        imagen: "../assets/images/Prod10.jpg",
     },
 ];
 
@@ -99,12 +99,13 @@ catch(e){
     console.log(e);
 }
 
+cargarProductos();
 
 //Evento para agregar a carrito en pantalla de detalle de producto. 
 function agregarACarrito()
 {
     const prods = JSON.parse(localStorage.getItem("listaProd") || "[]"); 
-    const nuevoObj = {'titulo' : document.querySelector('h1').innerHTML};
+    const nuevoObj = {'nombre' : document.querySelector('h1').innerHTML};
     prods.push(nuevoObj);
 
     localStorage.setItem("listaProd", JSON.stringify(prods)); 
@@ -124,5 +125,29 @@ function agregarACarrito()
           },
         stopOnFocus: true,
     }).showToast();
+}
+
+
+
+function cargarProductos()
+{
+    const contenedor = document.querySelector('#prodsCarrito');
+    const prodsDelCarrito = JSON.parse(localStorage.getItem("listaProd") || "[]"); 
+    prodsDelCarrito.forEach(function(prod) {
+        const div = document.createElement('div');
+        const masterSearch = masterProd.find(p => p.nombre === prod.nombre);
+        if (!masterSearch) return; // Si no se encuentra el producto, no hacer nada
+        div.classList.add('productoCheckout');
+        div.innerHTML = `
+            <img  src="${masterSearch.imagen}" alt="${masterSearch.nombre}">
+            <div class="w-75">
+                <h3 >${masterSearch.nombre}</h3>
+                <p class="descText">${masterSearch.descripcion}</p>
+                <p style="color:green">$${masterSearch.precio}</p>
+            </div>
+            <button  class="vw-10 btn miBotocarrito" onclick='eliminarProd()'">Eliminar</button>
+        `;
+        contenedor.appendChild(div);
+    });
 }
 
